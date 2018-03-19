b483c163e60cb1dceb20bdba8d7090f8456ce388
debashisnath25 <debashisnath1992@gmail.com>
2018-03-19 14:03:11 +0530

    header sidebar metalinks

---
+++ metalinks.php
@@ -0,0 +1,19 @@
+    <!--favicon icon-->
+    <link rel="icon" type="image/png" href="images/favicon.png">
+   
+    <!--google font-->
+    <link href="//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+
+    <!--common style-->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/lobicard.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/simple-line-icons.css" rel="stylesheet">
+    <link href="css/themify-icons.css" rel="stylesheet">
+    <link href="css/weather-icons.min.css" rel="stylesheet">
+
+    <!--custom css-->
+    <link href="css/main.min.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
